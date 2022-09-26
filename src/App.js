5203b3b32290752efd40d08eeba3864e6dd0e846
Cao Anh Van <19133067@student.hcmute.edu.vn>
2022-09-27 00:51:55 +0700

    end demo

--- src/App.js
+++ src/App.js
@@ -2,14 +2,15 @@ import logo from './logo.svg';
 import './App.css';
 import Parent from './components/Parent';
 import Sample from './components/Sample';
-
+import TodoList from './components/ToDoList';
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faTrash, faPlus, faEdit } from "@fortawesome/free-solid-svg-icons";
+library.add(faTrash, faEdit, faPlus);
 
 function App() {
   return (
     <div className='App'>
-      <h1>App component</h1>
-      <Parent></Parent>
-      <Sample></Sample>
+      <TodoList></TodoList>
     </div>
   );
 }
